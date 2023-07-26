d550f975fc827265c1fcbc9a0c30661fba126c22
Gyubong <jopemachine@naver.com>
2023-07-26 14:40:25 +0900

    Update 3-node-example.sh

--- misc/3-node-example.sh
+++ misc/3-node-example.sh
@@ -9,14 +9,14 @@ tmux split-window -v
 
 tmux select-pane -t 0
 sleep 0.5
-tmux send-keys 'python ./examples/memstore/main.py --raft-addr=0.0.0.0:5001 --web-server=0.0.0.0:8001' Enter
+tmux send-keys 'python ./examples/memstore/main.py --bootstrap --web-server=0.0.0.0:8001' Enter
 
 tmux select-pane -t 1
 sleep 0.5
-tmux send-keys 'sleep 2; and python ./examples/memstore/main.py --raft-addr=0.0.0.0:5002 --web-server=0.0.0.0:8002 --peer-addr=0.0.0.0:5001' Enter
+tmux send-keys 'sleep 2; and python ./examples/memstore/main.py --raft-addr=0.0.0.0:60062 --web-server=0.0.0.0:8002' Enter
 
 tmux select-pane -t 2
 sleep 0.5
-tmux send-keys 'sleep 3; and python ./examples/memstore/main.py --raft-addr=0.0.0.0:5003 --web-server=0.0.0.0:8003 --peer-addr=0.0.0.0:5001' Enter
+tmux send-keys 'sleep 3; and python ./examples/memstore/main.py --raft-addr=0.0.0.0:60003 --web-server=0.0.0.0:8003' Enter
 
 tmux select-pane -t 3
