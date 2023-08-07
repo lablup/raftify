4914529458364b6f3166367e3579a6b3ffd39ba4
Gyubong <jopemachine@naver.com>
2023-08-07 14:16:12 +0900

    Update API

--- Makefile
+++ Makefile
@@ -34,5 +34,5 @@ run-docker:
 
 publish:
 	rm -rf dist
-	python setup.py sdist bdist_wheel
+	python setup.py bdist_wheel
 	twine upload dist/*
--- raftify/__init__.py
+++ raftify/__init__.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 
+from .error import ClusterJoinError, UnknownError  # noqa: F401
 from .fsm import FSM  # noqa: F401
 from .lmdb import LMDBStorage, LMDBStorageCore  # noqa: F401
 from .mailbox import Mailbox  # noqa: F401
 from .message_sender import MessageSender  # noqa: F401
 from .raft_client import RaftClient  # noqa: F401
-from .raft_facade import RaftCluster  # noqa: F401
+from .raft_facade import FollowerRole, RaftCluster  # noqa: F401
 from .raft_node import RaftNode  # noqa: F401
 from .raft_server import RaftServer  # noqa: F401
+from .utils import PickleSerializer, SocketAddr  # noqa: F401
 
 __all__ = [
     "lmdb",
