cb9400e01aa36c145acc0548b800356ad4b3ec23
Gyubong <jopemachine@naver.com>
2023-09-09 14:21:49 +0900

    Fixed #32.

--- raftify/deserializer.py
+++ raftify/deserializer.py
@@ -2,6 +2,7 @@ import pickle
 from typing import Optional
 
 from rraft import (
+    ConfChange,
     set_confchange_context_deserializer,
     set_confchangev2_context_deserializer,
     set_entry_context_deserializer,
@@ -12,10 +13,32 @@ from rraft import (
 
 
 def pickle_deserialize(data: bytes) -> Optional[str]:
+    """
+    This function assumes that the given byte slice has been dumped with pickle and perform deserialization.
+    Note that if the given byte slice is not data dumped with pickle, but it contains data that has been dumped with pickle,
+    This returns only the dumped portion with pickle.
+    """
+
     if data == b"":
         return None
 
     if pickle.PROTO in data:
+        return pickle.loads(data[data.index(pickle.PROTO) :])
+
+    # Not pickle data
+    return data
+
+
+def set_entry_data_deserializer_cb(data: bytes) -> Optional[str]:
+    if data == b"":
+        return None
+
+    try:
+        return str(ConfChange.decode(data))
+    except Exception:
+        pass
+
+    if data.startswith(pickle.PROTO):
         return pickle.loads(data[data.index(pickle.PROTO) :])
 
     # Not pickle data
@@ -30,6 +53,6 @@ def init_rraft_py_deserializer():
     set_confchange_context_deserializer(pickle_deserialize)
     set_confchangev2_context_deserializer(pickle_deserialize)
     set_entry_context_deserializer(pickle_deserialize)
-    set_entry_data_deserializer(pickle_deserialize)
+    set_entry_data_deserializer(set_entry_data_deserializer_cb)
     set_message_context_deserializer(pickle_deserialize)
     set_snapshot_data_deserializer(pickle_deserialize)
