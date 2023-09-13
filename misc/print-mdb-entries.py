c5cbb81570162c1e3221c2a79cb5be05c156bf5b
Gyubong <jopemachine@naver.com>
2023-09-14 08:47:31 +0900

    Update print-mdb-entries.py

--- misc/print-mdb-entries.py
+++ misc/print-mdb-entries.py
@@ -3,6 +3,7 @@ import sys
 import lmdb
 import rraft
 from raftify.deserializer import init_rraft_py_deserializer
+from raftify.lmdb import SNAPSHOT_KEY, LAST_INDEX_KEY, HARD_STATE_KEY, CONF_STATE_KEY
 
 
 def main(argv):
@@ -13,12 +14,29 @@ def main(argv):
     env = lmdb.open(f"{os.getcwd()}/raft-{idx}.mdb", max_dbs=2)
 
     entries_db = env.open_db(b"entries")
-    # metadata_db = env.open_db(b"meta")
 
+    print('---- Entries ----')
     with env.begin(db=entries_db) as txn:
         cursor = txn.cursor()
         for key, value in cursor:
             print(f"Key: {int(key.decode())}, Value: {rraft.Entry.decode(value)}")
+
+    metadata_db = env.open_db(b"meta")
+
+    print('---- Metadata ----')
+    with env.begin(db=metadata_db) as txn:
+        cursor = txn.cursor()
+        for key, value in cursor:
+            if key == SNAPSHOT_KEY:
+                print(f'Key: "snapshot", Value: "{rraft.Snapshot.decode(value)}"')
+            elif key == LAST_INDEX_KEY:
+                print(f'Key: "last_index", Value: "{int(value.decode())}"')
+            elif key == HARD_STATE_KEY:
+                print(f'Key: "hard_state", Value: "{rraft.HardState.decode(value)}"')
+            elif key == CONF_STATE_KEY:
+                print(f'Key: "conf_state", Value: "{rraft.ConfState.decode(value)}"')
+            else:
+                assert False, f"Unknown key: {key}"
 
     env.close()
 
