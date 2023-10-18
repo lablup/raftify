95b44f7d0082d455489d1d3010e3e39708cc4831
Gyubong <jopemachine@naver.com>
2023-10-18 10:00:04 +0900

    Update setup_logger

--- tests/harness/logger.py
+++ tests/harness/logger.py
@@ -1,11 +1,25 @@
 import logging
 import os
+import colorlog
 
 from rraft import default_logger
 
 
 def setup_logger() -> logging.Logger:
-    logging.basicConfig(level=logging.DEBUG)
+    log_format = "%(asctime)s - " "%(log_color)s%(levelname)-8s - %(message)s%(reset)s"
+
+    log_colors_config = {
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red",
+        "asctime": "grey",
+    }
+
+    colorlog.basicConfig(
+        level=logging.DEBUG, format=log_format, log_colors=log_colors_config
+    )
     return logging.getLogger()
 
 
