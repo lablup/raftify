3a2814cc777bdae556af521eb8738ae896bafa66
Gyubong <jopemachine@naver.com>
2023-10-29 14:26:22 +0900

    Update fsm.py

--- raftify/fsm.py
+++ raftify/fsm.py
@@ -2,6 +2,12 @@ import abc
 
 
 class FSM(metaclass=abc.ABCMeta):
+    """
+    A Finite State Machine (FSM) abstract class to be utilized within a Raft framework.
+    This class is designed to apply commands to a state, take snapshots of the state,
+    and restore the state from a snapshot.
+    """
+
     @abc.abstractmethod
     async def apply(self, message: bytes) -> bytes:
         raise NotImplementedError
