4f6bad7422d2420fe6df1e19f96655bcdd0f9188
Gyubong <jopemachine@naver.com>
2023-11-09 12:40:33 +0900

    Add timeout default value to RaftClient

--- raftify/raft_client.py
+++ raftify/raft_client.py
@@ -67,7 +67,7 @@ class RaftClient:
             )
 
     async def send_message(
-        self, msg: Message, timeout: float
+        self, msg: Message, timeout: float = 5.0
     ) -> raft_service_pb2.SendMessageResponse:
         """
         Request to send a message to the cluster.
@@ -80,7 +80,7 @@ class RaftClient:
             return await asyncio.wait_for(stub.SendMessage(request), timeout)
 
     async def propose(
-        self, data: bytes, timeout: float
+        self, data: bytes, timeout: float = 5.0
     ) -> raft_service_pb2.ProposeResponse:
         """
         Request to send a propose to the cluster.
@@ -91,7 +91,7 @@ class RaftClient:
             return await asyncio.wait_for(stub.Propose(data), timeout)
 
     async def request_id(
-        self, addr: SocketAddr, timeout: float
+        self, addr: SocketAddr, timeout: float = 5.0
     ) -> raft_service_pb2.IdRequestResponse:
         """
         Request to reserve an ID for a new node to join the cluster.
@@ -104,12 +104,12 @@ class RaftClient:
             return await asyncio.wait_for(stub.RequestId(request_args), timeout)
 
     async def member_bootstrap_ready(
-        self, follower_id: int, timeout: float
+        self, follower_id: int, timeout: float = 5.0
     ) -> raft_service_pb2.MemberBootstrapReadyResponse:
         """
         Request to notify that a follower is ready to bootstrap.
-        From follower to leader.
-        Used in initial cluster bootstrap.
+        Made from follower to leader.
+        Used in the initial cluster bootstrap.
         """
 
         request_args = raft_service_pb2.MemberBootstrapReadyArgs(
@@ -123,12 +123,12 @@ class RaftClient:
             )
 
     async def cluster_bootstrap_ready(
-        self, peers: bytes, timeout: float
+        self, peers: bytes, timeout: float = 5.0
     ) -> raft_service_pb2.ClusterBootstrapReadyResponse:
         """
         Request to notify that a leader is ready to bootstrap the cluster.
-        From leader to follower.
-        Used in initial cluster bootstrap.
+        Made from leader to follower.
+        Used in the initial cluster bootstrap.
         """
 
         request_args = raft_service_pb2.ClusterBootstrapReadyArgs(peers=peers)
@@ -142,7 +142,7 @@ class RaftClient:
     async def reroute_message(
         self,
         reroute_msg_type: raft_service_pb2.RerouteMsgType,
-        timeout: float,
+        timeout: float = 5.0,
         msg_bytes: Optional[bytes] = None,
         conf_change: Optional[eraftpb_pb2.ConfChangeV2] = None,
     ) -> raft_service_pb2.SendMessageResponse:
@@ -160,7 +160,9 @@ class RaftClient:
             stub = raft_service_pb2_grpc.RaftServiceStub(channel)
             return await asyncio.wait_for(stub.RerouteMessage(request), timeout)
 
-    async def debug_node(self, timeout: float) -> raft_service_pb2.DebugNodeResponse:
+    async def debug_node(
+        self, timeout: float = 5.0
+    ) -> raft_service_pb2.DebugNodeResponse:
         """
         Request to debug the node.
         """
@@ -172,7 +174,7 @@ class RaftClient:
             return await asyncio.wait_for(stub.DebugNode(request), timeout)
 
     async def debug_entries(
-        self, timeout: float
+        self, timeout: float = 5.0
     ) -> raft_service_pb2.DebugEntriesResponse:
         """
         Request to debug the node.
@@ -184,9 +186,9 @@ class RaftClient:
             stub = raft_service_pb2_grpc.RaftServiceStub(channel)
             return await asyncio.wait_for(stub.DebugEntries(request), timeout)
 
-    async def version(self, timeout: float) -> raft_service_pb2.VersionResponse:
+    async def version(self, timeout: float = 5.0) -> raft_service_pb2.VersionResponse:
         """
-        Request to debug raftify version.
+        Request to get RaftServer's raftify version.
         """
 
         request = raft_service_pb2.Empty()
