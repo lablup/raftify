f911015885e7fdc2d5b33f8a9d33323bb8a224c2
Gyubong <jopemachine@naver.com>
2023-11-16 15:39:37 +0900

    Update raft_node.py

--- raftify/raft_node.py
+++ raftify/raft_node.py
@@ -324,7 +324,7 @@ class RaftNode:
     def handle_node_auto_removal(self, elapsed: float, node_id: int) -> None:
         assert self.is_leader()
 
-        if node_id not in self.peers.keys():
+        if node_id not in self.peers:
             return
 
         if elapsed >= self.raftify_cfg.node_auto_remove_threshold:
@@ -342,7 +342,7 @@ class RaftNode:
 
     def should_retry(self, node_id: int, current_retry_count: int) -> bool:
         """ """
-        if node_id not in self.peers.keys():
+        if node_id not in self.peers:
             return False
 
         return current_retry_count < self.raftify_cfg.max_retry_cnt
@@ -414,7 +414,7 @@ class RaftNode:
 
                 if elapsed < self.raftify_cfg.node_auto_remove_threshold:
                     self.logger.warning(
-                        f"Failed to connect to node {node_id}, elapsed from first failure: {format(elapsed, '.4f')}s"
+                        f"Failed to connect to node {node_id}, elapsed from first failure: {format(elapsed, '.4f')}s. Err message: {str(err)}"
                     )
 
                 if self.is_leader():
