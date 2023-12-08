394bb7d2f9bb5b2972862294a20e74c05c8ec12a
Gyubong <jopemachine@naver.com>
2023-12-08 12:28:33 +0900

    Remove memstore useless config

--- examples/memstore/src/utils.rs
+++ examples/memstore/src/utils.rs
@@ -18,36 +18,3 @@ pub fn build_config() -> Config {
     cfg.raft_config = raft_cfg;
     return cfg;
 }
-
-#[derive(Deserialize)]
-pub struct RaftPeer {
-    pub ip: String,
-    pub port: u16,
-    pub node_id: u64,
-}
-
-#[derive(Deserialize)]
-pub struct RaftConfiguration {
-    peers: Vec<RaftPeer>,
-}
-
-#[derive(Deserialize)]
-pub struct Configuration {
-    cfg: RaftConfiguration,
-}
-
-pub async fn load_peers() -> Result<Peers, Box<dyn std::error::Error>> {
-    let path = Path::new(file!()).parent().unwrap().join("config.toml");
-    let config_str = fs::read_to_string(path)?;
-
-    let raft_config: Configuration = toml::from_str(&config_str)?;
-
-    let mut peers = Peers::new();
-
-    for peer_info in raft_config.cfg.peers {
-        let addr = SocketAddr::new(peer_info.ip.parse().unwrap(), peer_info.port);
-        peers.add_peer(peer_info.node_id, addr);
-    }
-
-    Ok(peers)
-}
