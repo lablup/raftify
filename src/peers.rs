01bb93ec0df02e619cfa5f45c8c1dc7225441fab
Gyubong <jopemachine@naver.com>
2023-12-08 12:28:13 +0900

    Update peers.rs

--- src/peers.rs
+++ src/peers.rs
@@ -6,7 +6,7 @@ use crate::Peer;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Peers {
-    pub peers: HashMap<u64, Peer>,
+    pub inner: HashMap<u64, Peer>,
 }
 
 impl Default for Peers {
@@ -18,41 +18,41 @@ impl Default for Peers {
 impl Peers {
     pub fn new() -> Self {
         Self {
-            peers: HashMap::new(),
+            inner: HashMap::new(),
         }
     }
 
     pub fn is_empty(&self) -> bool {
-        self.peers.is_empty()
+        self.inner.is_empty()
     }
 
     pub fn get(&self, id: &u64) -> Option<&Peer> {
-        self.peers.get(&id)
+        self.inner.get(&id)
     }
 
     pub fn get_mut(&mut self, id: &u64) -> Option<&mut Peer> {
-        self.peers.get_mut(&id)
+        self.inner.get_mut(&id)
     }
 
     pub fn remove(&mut self, id: &u64) -> Option<Peer> {
-        self.peers.remove(&id)
+        self.inner.remove(&id)
     }
 
     pub fn add_peer<A: ToSocketAddrs>(&mut self, id: u64, addr: A) {
         let addr = addr.to_socket_addrs().unwrap().next().unwrap();
         let peer = Peer::new(addr);
-        self.peers.insert(id, peer);
+        self.inner.insert(id, peer);
     }
 
     pub fn reserve_peer(&mut self, self_id: u64) -> u64 {
-        let next_id = self.peers.keys().max().cloned().unwrap_or(1);
+        let next_id = self.inner.keys().max().cloned().unwrap_or(1);
         let next_id = std::cmp::max(next_id + 1, self_id);
         next_id
     }
 
     pub fn get_node_id_by_addr<A: ToSocketAddrs>(&self, addr: A) -> Option<u64> {
         let addr = addr.to_socket_addrs().unwrap().next().unwrap();
-        self.peers
+        self.inner
             .iter()
             .find(|(_, peer)| peer.addr == addr)
             .map(|(id, _)| *id)
