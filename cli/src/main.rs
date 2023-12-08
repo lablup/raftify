6b5abf9d252758117da6236a5679992604156223
Gyubong <jopemachine@naver.com>
2023-12-09 00:05:39 +0900

    Update main.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -12,35 +12,61 @@ fn main() {
             SubCommand::with_name("debug")
                 .about("Debug tools")
                 .subcommand(
-                    SubCommand::with_name("entries")
-                        .about("List persisted log entries")
+                    SubCommand::with_name("persisted")
+                        .about("List persisted log entries and metadata")
                         .arg(
-                            Arg::with_name("address")
-                                .help("The address to connect to")
+                            Arg::with_name("path")
+                                .help("The log dir path")
                                 .required(true)
                                 .index(1),
                         ),
                 )
                 .subcommand(
-                    SubCommand::with_name("node").about("Manage nodes").arg(
+                    SubCommand::with_name("entries")
+                        .about("List all log entries")
+                        .arg(
+                            Arg::with_name("address")
+                                .help("The address of the RaftNode")
+                                .required(true)
+                                .index(1),
+                        ),
+                )
+                .subcommand(
+                    SubCommand::with_name("node").about("Inspect RaftNode").arg(
                         Arg::with_name("address")
-                            .help("The address to connect to for node")
+                            .help("The address of the RaftNode")
                             .required(true)
                             .index(1),
                     ),
                 ),
         )
+        // .subcommand(SubCommand::with_name("health").about("Check health"))
         .get_matches();
 
-    if let Some(debug_matches) = matches.subcommand_matches("debug") {
-        if let Some(entries_matches) = debug_matches.subcommand_matches("entries") {
-            if let Some(address) = entries_matches.value_of("address") {
-                println!("Running 'entries' for address: {}", address);
-            }
-        } else if let Some(node_matches) = debug_matches.subcommand_matches("node") {
-            if let Some(address) = node_matches.value_of("address") {
-                println!("Running 'node' for address: {}", address);
+    match matches.subcommand() {
+        Some(("debug", debug_matches)) => {
+            match debug_matches.subcommand() {
+                Some(("entries", entries_matches)) => {
+                    if let Some(address) = entries_matches.value_of("address") {
+                        println!("Running 'entries' for address: {}", address);
+                        // 'entries' 서브커맨드 로직 구현
+                    }
+                }
+                Some(("node", node_matches)) => {
+                    if let Some(address) = node_matches.value_of("address") {
+                        println!("Running 'node' for address: {}", address);
+                        // 'node' 서브커맨드 로직 구현
+                    }
+                }
+                Some(("persisted", node_matches)) => {
+                    if let Some(path) = node_matches.value_of("path") {
+                        println!("Running 'node' for address: {}", path);
+                        // 'persisted' 서브커맨드 로직 구현
+                    }
+                }
+                _ => {}
             }
         }
+        _ => {}
     }
 }
