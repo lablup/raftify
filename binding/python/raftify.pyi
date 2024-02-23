a15594c35d23076ba86992c2b66be89b99d8dc95
Gyubong <jopemachine@naver.com>
2024-02-23 11:45:00 +0900

    [python] Delete obsolete API

--- binding/python/raftify.pyi
+++ binding/python/raftify.pyi
@@ -131,8 +131,6 @@ class RaftNode:
         """ """
     async def get_cluster_size(self) -> int:
         """ """
-    async def set_bootstrap_done(self) -> None:
-        """ """
     async def store(self) -> "AbstractStateMachine":
         """ """
 
