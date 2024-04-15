8cf98df53ea64b3a8a71fae0b71a1f765fbc3d21
Gyubong <jopemachine@naver.com>
2024-04-15 18:16:52 +0900

    Fix bootstrap from WAL snapshot not working bug

--- raftify/src/raft_node/mod.rs
+++ raftify/src/raft_node/mod.rs
@@ -1388,6 +1388,16 @@ impl<
     }
 
     async fn on_ready(&mut self) -> Result<()> {
+        // TODO: Improve this logic.
+        // I'd like to move this logic into RaftNodeCore::bootstrap, but it's currently not possible because it is not async.
+        if let Some(_) = self.config.restore_wal_snapshot_from {
+            self.logger.info("Restoring state machine from snapshot...");
+            let store = self.raw_node.store();
+            let snap = store.snapshot(0, store.last_index()?)?;
+            self.fsm.restore(snap.get_data().to_vec()).await?;
+            self.config.restore_wal_snapshot_from = None;
+        }
+
         if !self.raw_node.has_ready() {
             return Ok(());
         }
