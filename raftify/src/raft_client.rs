e39f30a65bdf567b3956cb269db8df5923d3dc0c
KIMJiho <rlawlgh1028@naver.com>
2024-08-17 14:45:33 +0900

    add https schema

--- raftify/src/raft_client.rs
+++ raftify/src/raft_client.rs
@@ -4,19 +4,36 @@ use tonic::transport::{Channel, Error as TonicError};
 
 use super::RaftServiceClient;
 
-// TODO: Support https schema
 pub async fn create_client<A: ToSocketAddrs>(
     addr: A,
 ) -> Result<RaftServiceClient<Channel>, TonicError> {
+    _create_client(addr, false).await
+}
+
+pub async fn create_client_with_ssl<A: ToSocketAddrs>(
+    addr: A,
+) -> Result<RaftServiceClient<Channel>, TonicError> {
+    _create_client(addr, true).await
+}
+
+async fn _create_client<A: ToSocketAddrs>(
+    addr: A,
+    b_ssl: bool,
+) -> Result<RaftServiceClient<Channel>, TonicError> {
+    let httpx = if b_ssl {
+        String::from("https")
+    } else {
+        String::from("http")
+    };
     let addr = addr
         .to_socket_addrs()
         .expect("Invalid socket address format")
         .next()
         .unwrap();
-    let addr = format!("http://{}", addr);
-    let addr = Bytes::copy_from_slice(addr.as_bytes());
+    let url = format!("{}://{}", httpx, addr);
+    let url = Bytes::copy_from_slice(url.as_bytes());
 
-    let channel = Channel::from_shared(addr).unwrap().connect().await?;
+    let channel = Channel::from_shared(url).unwrap().connect().await?;
     let client = RaftServiceClient::new(channel);
 
     Ok(client)
