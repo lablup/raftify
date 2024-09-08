6795dfb0ea5dcb59bb313d41b6d7822ef1a487da
KIMJiho <rlawlgh1028@naver.com>
2024-09-08 12:08:27 +0900

    writing test...

--- raftify/src/raft_client.rs
+++ raftify/src/raft_client.rs
@@ -38,3 +38,12 @@ async fn _create_client<A: ToSocketAddrs>(
 
     Ok(client)
 }
+
+// Todo!
+#[cfg(test)]
+mod test {
+    #[tokio::test]
+    async fn test_create_client() {
+        todo!("!!!")
+    }
+}
