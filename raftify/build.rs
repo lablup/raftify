38667f7df9f641addfb4fdefeb104af5cf9d7279
Gyubong <jopemachine@naver.com>
2024-10-05 16:45:48 +0900

    fix: CI

--- raftify/build.rs
+++ raftify/build.rs
@@ -5,7 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .extern_path(".eraftpb", "::raft::eraftpb")
         .compile(&["proto/raft_service.proto"], &["proto/"])?;
 
-
     built::write_built_file().expect("Failed to acquire build-time information");
     Ok(())
 }
