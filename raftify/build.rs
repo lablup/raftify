74fb1a174559767ca9099ef0f2bd0c2285ceab3e
Gyubong <jopemachine@naver.com>
2024-10-05 15:51:34 +0900

    etc: Test if formatter is running

--- raftify/build.rs
+++ raftify/build.rs
@@ -5,6 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .extern_path(".eraftpb", "::raft::eraftpb")
         .compile(&["proto/raft_service.proto"], &["proto/"])?;
 
+
     built::write_built_file().expect("Failed to acquire build-time information");
     Ok(())
 }
