f1da1f3833409b60c74adc2dc2efd808cfba6f64
Gyubong <jopemachine@naver.com>
2024-10-06 10:54:13 +0900

    refactor: Use comft-table for autowrap

--- raftify-cli/src/commands/debug.rs
+++ raftify-cli/src/commands/debug.rs
@@ -1,5 +1,5 @@
+use comfy_table::{Cell, CellAlignment, ContentArrangement, Table};
 use core::panic;
-use prettytable::*;
 use serde_json::Value;
 use std::{collections::HashMap, fs, path::Path, sync::Arc};
 
@@ -14,7 +14,11 @@ use raftify::{
     raft_service, ConfigBuilder, HeedStorage, Result, StableStorage, StorageType,
 };
 
-pub fn debug_persisted<LogStorage: StableStorage>(path: &str, logger: slog::Logger) -> Result<()> {
+pub fn debug_persisted<LogStorage: StableStorage>(
+    path: &str,
+    logger: slog::Logger,
+    print_as_table: bool,
+) -> Result<()> {
     let config = ConfigBuilder::new().log_dir(path.to_string()).build();
 
     let storage = match LogStorage::STORAGE_TYPE {
@@ -43,22 +47,40 @@ pub fn debug_persisted<LogStorage: StableStorage>(path: &str, logger: slog::Logg
     }
 
     println!("---- Persisted entries ----");
-    let mut table = Table::new();
-    let formatter = CUSTOM_FORMATTER.read().unwrap();
 
-    table.add_row(row!["index", "type", "context", "data", "term"]);
+    if print_as_table {
+        let mut table = Table::new();
+        let formatter = CUSTOM_FORMATTER.read().unwrap();
 
-    for entry in entries.iter() {
-        println!("Key: {}, {:?}", entry.get_index(), format_entry(entry));
-        table.add_row(row![
-            entry.get_index(),
-            format!("{:?}", entry.get_entry_type()).replace("Entry", ""),
-            formatter.format_entry_context(&entry.context.clone().into()),
-            formatter.format_entry_data(&entry.data.clone().into()),
-            entry.get_term()
-        ]);
+        table
+            .set_header(vec![
+                Cell::new("index").set_alignment(CellAlignment::Center),
+                Cell::new("type").set_alignment(CellAlignment::Center),
+                Cell::new("data").set_alignment(CellAlignment::Center),
+                Cell::new("term").set_alignment(CellAlignment::Center),
+            ])
+            .set_content_arrangement(ContentArrangement::Dynamic);
+
+        for entry in entries.iter() {
+            table.add_row(vec![
+                Cell::new(entry.get_index()).set_alignment(CellAlignment::Center),
+                Cell::new(
+                    format!("{:?}", entry.get_entry_type())
+                        .replace("Entry", "")
+                        .replace("ConfChangeV2", "ConfChange"),
+                )
+                .set_alignment(CellAlignment::Center),
+                Cell::new(formatter.format_entry_data(&entry.data.clone().into()))
+                    .set_alignment(CellAlignment::Left),
+                Cell::new(entry.get_term()).set_alignment(CellAlignment::Center),
+            ]);
+        }
+        println!("{}", table);
+    } else {
+        for entry in entries.iter() {
+            println!("Key: {}, {:?}", entry.get_index(), format_entry(entry));
+        }
     }
-    table.printstd();
 
     println!();
 
@@ -73,6 +95,7 @@ pub fn debug_persisted<LogStorage: StableStorage>(path: &str, logger: slog::Logg
 pub fn debug_persisted_all<LogStorage: StableStorage>(
     path_str: &str,
     logger: slog::Logger,
+    print_as_table: bool,
 ) -> Result<()> {
     let path = match fs::canonicalize(Path::new(&path_str)) {
         Ok(absolute_path) => absolute_path,
@@ -104,7 +127,11 @@ pub fn debug_persisted_all<LogStorage: StableStorage>(
 
         for name in dir_entries {
             println!("*----- {name} -----*");
-            debug_persisted::<LogStorage>(&format!("{}/{}", path_str, name), logger.clone())?;
+            debug_persisted::<LogStorage>(
+                &format!("{}/{}", path_str, name),
+                logger.clone(),
+                print_as_table,
+            )?;
             println!();
         }
     } else {
