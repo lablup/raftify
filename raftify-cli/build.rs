4e7316465f929c1f9af20fdf0a9ee21eced9ca3e
Gyubong Lee <jopemachine@naver.com>
2024-10-15 12:46:00 +0900

    refactor: Replace debug commands with describe commands (#168)

--- raftify-cli/build.rs
+++ raftify-cli/build.rs
@@ -4,13 +4,24 @@ use toml::Value;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new("Cargo.toml");
-    let read = fs::read_to_string(path)
-        .expect("Failed to read Cargo.toml");
+    let read = fs::read_to_string(path).expect("Failed to read Cargo.toml");
     let toml: Value = read.parse::<Value>().expect("Failed to parse Cargo.toml");
 
-    if let Some(raftify_dep) = toml.get("dependencies").and_then(|deps| deps.get("raftify")) {
+    if let Some(raftify_dep) = toml
+        .get("dependencies")
+        .and_then(|deps| deps.get("raftify"))
+    {
         let version = raftify_dep.get("version").unwrap().as_str().unwrap();
-        let features = raftify_dep.get("features").unwrap().as_array().unwrap().iter().map(|f| f.as_str().unwrap()).collect::<Vec<_>>().join(", ");
+        let features = raftify_dep
+            .get("features")
+            .unwrap()
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|f| f.as_str().unwrap())
+            .collect::<Vec<_>>()
+            .join(", ");
+
         println!("cargo:rustc-env=RAFTIFY_VERSION={}", &version[1..]);
         println!("cargo:rustc-env=RAFTIFY_FEATURES={}", features);
     }
--- raftify-cli/src/commands/describe.rs
+++ raftify-cli/src/commands/describe.rs
@@ -1,12 +1,8 @@
-use comfy_table::{
-    presets::UTF8_FULL, Cell, CellAlignment, ContentArrangement, Table,
-};
+use comfy_table::{presets::UTF8_FULL, Cell, CellAlignment, ContentArrangement, Table};
 use core::panic;
 use serde_json::Value;
 use std::{
     collections::{BTreeSet, HashMap},
-    fs,
-    path::Path,
     sync::Arc,
 };
 
@@ -21,10 +17,10 @@ use raftify::{
     raft_service, ConfigBuilder, HeedStorage, Result, StableStorage, StorageType,
 };
 
-pub fn debug_persisted<LogStorage: StableStorage>(
+pub fn describe_entries<LogStorage: StableStorage>(
     path: &str,
     logger: slog::Logger,
-    print_as_table: bool,
+    print_raw_format: bool,
 ) -> Result<()> {
     let config = ConfigBuilder::new().log_dir(path.to_string()).build();
 
@@ -53,9 +49,11 @@ pub fn debug_persisted<LogStorage: StableStorage>(
         );
     }
 
-    println!("---- Persisted entries ----");
-
-    if print_as_table {
+    if print_raw_format {
+        for entry in entries.iter() {
+            println!("{}", format_entry(entry));
+        }
+    } else {
         let mut table = Table::new();
         table
             .load_preset(UTF8_FULL)
@@ -87,28 +85,54 @@ pub fn debug_persisted<LogStorage: StableStorage>(
             ]);
         }
         println!("{}", table);
-    } else {
-        for entry in entries.iter() {
-            println!("Key: {}, {:?}", entry.get_index(), format_entry(entry));
-        }
     }
 
     println!();
 
-    println!("---- Metadata ----");
+    Ok(())
+}
+
+pub fn describe_metadata<LogStorage: StableStorage>(
+    path: &str,
+    logger: slog::Logger,
+    print_raw_format: bool,
+) -> Result<()> {
+    let config = ConfigBuilder::new().log_dir(path.to_string()).build();
+
+    let storage = match LogStorage::STORAGE_TYPE {
+        StorageType::Heed => HeedStorage::create(
+            config.get_log_dir(),
+            &config,
+            Arc::new(Slogger {
+                slog: logger.clone(),
+            }),
+        )?,
+        StorageType::InMemory => {
+            panic!("InMemory storage does not support this feature");
+        }
+        _ => {
+            panic!("Unsupported storage type");
+        }
+    };
+
     let hard_state = storage.hard_state()?;
     let conf_state = storage.conf_state()?;
     let snapshot = storage.snapshot(0, 0)?;
     let last_index = storage.last_index()?;
 
-    if print_as_table {
+    if print_raw_format {
+        println!("{:?}", storage.hard_state()?);
+        println!("{:?}", storage.conf_state()?);
+        println!("{:?}", format_snapshot(&storage.snapshot(0, 0)?));
+        println!("Last index: {}", storage.last_index()?);
+    } else {
         let mut table = Table::new();
         table
             .load_preset(UTF8_FULL)
             .set_content_arrangement(ContentArrangement::Dynamic);
 
         table.set_header(vec![
-            Cell::new("Category"),
+            Cell::new(""),
             Cell::new("Field"),
             Cell::new("Value"),
         ]);
@@ -233,66 +257,12 @@ pub fn debug_persisted<LogStorage: StableStorage>(
         ]);
 
         println!("{}", table);
-    } else {
-        println!("{:?}", storage.hard_state()?);
-        println!("{:?}", storage.conf_state()?);
-        println!("{:?}", format_snapshot(&storage.snapshot(0, 0)?));
-        println!("Last index: {}", storage.last_index()?);
     }
 
     Ok(())
 }
 
-pub fn debug_persisted_all<LogStorage: StableStorage>(
-    path_str: &str,
-    logger: slog::Logger,
-    print_as_table: bool,
-) -> Result<()> {
-    let path = match fs::canonicalize(Path::new(&path_str)) {
-        Ok(absolute_path) => absolute_path,
-        Err(e) => {
-            panic!("Invalid path: {}", e);
-        }
-    };
-
-    if let Ok(subdirectories) = fs::read_dir(path.clone()) {
-        let mut dir_entries = vec![];
-        for dir_entry in subdirectories.filter_map(|e| e.ok()) {
-            let path = dir_entry.path();
-            if path.is_dir() {
-                if let Some(file_name) = path.file_name() {
-                    if let Some(name) = file_name.to_str() {
-                        if name.starts_with("node-") {
-                            dir_entries.push(name.to_owned());
-                        }
-                    }
-                }
-            }
-        }
-
-        if dir_entries.is_empty() {
-            panic!("Raft node log directory not found: \"{}\"", path.display());
-        }
-
-        dir_entries.sort();
-
-        for name in dir_entries {
-            println!("*----- {name} -----*");
-            debug_persisted::<LogStorage>(
-                &format!("{}/{}", path_str, name),
-                logger.clone(),
-                print_as_table,
-            )?;
-            println!();
-        }
-    } else {
-        panic!("Failed to read subdirectory: {}", path.display());
-    }
-
-    Ok(())
-}
-
-pub async fn debug_node(addr: &str) -> Result<()> {
+pub async fn describe_node(addr: &str) -> Result<()> {
     // TODO: Support TLS configuration
     let mut client = create_client(&addr, None).await?;
     let response = client.debug_node(raft_service::Empty {}).await?;
@@ -301,8 +271,4 @@ pub async fn debug_node(addr: &str) -> Result<()> {
 
     println!("{}", format_debugging_info(&parsed));
     Ok(())
-}
-
-pub async fn debug_entries(_addr: &str) -> Result<()> {
-    todo!()
 }
